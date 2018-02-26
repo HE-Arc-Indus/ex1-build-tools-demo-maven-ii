63dd0c59afe887283d1553718a2dfaf67804116a
odilee <odile.eichenbe@he-arc.ch>
2018-02-26 11:58:57 +0100

    First
    first commit

---
+++ MesPersonnes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.sda.personne;
+
+/**
+ *
+ * @author odile.eichenbe
+ */
+public class MesPersonnes {
+    
+    public static void main(String[] args) {
+        
+        Personne personne = new Personne();
+        Personne pe = new Personne();
+        personne.setNom("Eichi");
+        personne.setPrenom("Odile");
+        personne.sePresenter();
+    }
+}
